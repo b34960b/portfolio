9c12b8c8fc45ee2b9b25f66296df9d4c3e5a6602
b34960b@gmail.com <chugyeonglim@chugyeonglim-ui-MacBookPro.local>
2020-06-09 19:55:35 +0900

    portfolio

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 (function($){
 
-  const imgBox = $('.imgBox');
-  const rImgBox = imgBox.find('.r-imgBox');
-  const rTextArea = $('.r-textArea');
+  // const imgBox = $('.imgBox');
+  // const rImgBox = imgBox.find('.r-imgBox');
+  // const rTextArea = $('.r-textArea');
 
-  rImgBox.on('mouseenter', function(){
-    rTextArea.css({'display':'block'});
-    rTextArea.animate({'transition': 'all 500ms ease'});
-  });
-  rImgBox.on('mouseleave', function(){
-    rTextArea.css({display:'none'});
-  });
+  // rImgBox.on('mouseenter', function(){
+  //   rTextArea.css({'display':'block'});
+  //   rTextArea.animate({'transition': 'all 500ms ease'});
+  // });
+  // rImgBox.on('mouseleave', function(){
+  //   rTextArea.css({display:'none'});
+  // });
 
 
 
